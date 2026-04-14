3afe6cb832a7a6750cc7f0a3228b535159ff57c6
Chronoss0518 <sinyusuke5000@gmail.com>
2026-04-14 14:56:16 +0900

    Menuの土台を作成

---
+++ lib/component/menu.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const HOME_KEY = "/home";
+const PROFILE_KEY = "/profile";
+
+class HeaderMenu extends StatelessWidget
+{
+
+  HeaderMenu(this.pageKey,{super.key});
+
+  final String pageKey;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
