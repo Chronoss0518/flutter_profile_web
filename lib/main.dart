400a02d01d4e4e8c437a83c2e5964169388c5207
Chronoss0518 <sinyusuke5000@gmail.com>
2026-04-14 15:00:04 +0900

    main.dartにHome画面とProfile画面を登録

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_profile_homepage/component/menu.dart';
+import 'package:flutter_profile_homepage/display/profile.dart';
+
+import 'display/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes:<String, WidgetBuilder>{
+        HOME_KEY :(BuildContext context) {return const Home();},
+        PROFILE_KEY :(BuildContext context) {return const Profile();},
+      },
+      initialRoute: HOME_KEY,
+    );
+  }
+}
