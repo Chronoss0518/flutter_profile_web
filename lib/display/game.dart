b6756e70175ecd98e3a576de88147bc9eea7bfa1
Chronoss0518 <sinyusuke5000@gmail.com>
2026-04-18 20:39:45 +0900

    Game画面用の枠組みを仮作成

---
+++ lib/display/game.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_profile_homepage/component/menu.dart';
+import 'package:flutter_profile_homepage/display/display_base.dart';
+
+const GAME_TITLE = "Home";
+const GAME_THAME_COLOR = Colors.yellow;
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _State();
+}
+
+class _State extends State<Game> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DisplayBase(
+      Center(
+      ),
+      GAME_KEY,
+      GAME_TITLE,
+      GAME_THAME_COLOR,
+    );
+  }
+}
