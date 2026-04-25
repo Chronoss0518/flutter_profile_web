1d584eff63a8e7f0a93fa44a0557a96d4b2a5bb8
Chronoss0518 <sinyusuke5000@gmail.com>
2026-04-25 22:01:38 +0900

    簡単な紹介文を追加

--- lib/display/home.dart
+++ lib/display/home.dart
@@ -16,9 +16,15 @@ const _ALINE_SIZE = 95.0 / EXPECTED_WIDTH;
 const _PANEL_WIDTH = 300.0 / EXPECTED_WIDTH;
 const _PANEL_HEIGHT = 300.0 / EXPECTED_HEIGHT;
 
-const _TOP_TEXT_TOP_ALINE = 87.0 / EXPECTED_HEIGHT;
-const _TOP_TEXT_BOTTOM_ALINE = 70.0 / EXPECTED_HEIGHT;
+const _TOP_TEXT_TOP_ALINE = 35.0 / EXPECTED_HEIGHT;
 const _TOP_TEXT_SIZE = 32.0 / EXPECTED_HEIGHT;
+const _TOP_TEXT_HEIGHT = 39.0 / EXPECTED_HEIGHT;
+
+const _DESCRIPTION_TOP_ALINE = 17.0 / EXPECTED_HEIGHT;
+const _DESCRIPTION_SIZE = 24.0 / EXPECTED_HEIGHT;
+const _DESCRIPTION_HEIGHT = 87.0 / EXPECTED_HEIGHT;
+
+const _PANEL_TOP_ALINE = 18.0 / EXPECTED_HEIGHT;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -39,16 +45,30 @@ class _State extends State<Home> {
     return DisplayBase(
       Column(children: [
         Container(
-          margin: EdgeInsets.only(top:_TOP_TEXT_TOP_ALINE * size.height,bottom:_TOP_TEXT_BOTTOM_ALINE * size.height),
+          margin: EdgeInsets.only(top:_TOP_TEXT_TOP_ALINE * size.height),
+          height: _TOP_TEXT_HEIGHT  * size.height,
           child: Text("Chronossの紹介ページ",style: TextStyle(fontSize: _TOP_TEXT_SIZE * size.height),),
         ),
-        Center(
-          child: Row(
-            children: [
-            _createPanels(context,PROFILE_KEY,PROFILE_THAME_COLOR,PROFILE_TITLE,"より詳細な情報を確認できます。"),
-            _createPanels(context,GAME_KEY,GAME_THAME_COLOR,GAME_TITLE,"Chronossの制作したゲームを確認できます。"),
-            _createPanels(context,LIBRARY_KEY,LIBRARY_THAME_COLOR,LIBRARY_TITLE,"Chronossの制作したライブラリを確認できます。"),
-          ],),
+        Container(
+          margin: EdgeInsets.only(top:_DESCRIPTION_TOP_ALINE * size.height),
+          height: _DESCRIPTION_HEIGHT  * size.height,
+          child: Text(
+            '''
+Chronossは個人でゲームを作っている一人です。専門学生時代からライブラリの構築を始め、
+それを利用したゲームやUnity・Unreal Engineを用いたゲーム開発も行う事があります。
+            '''
+            ,style: TextStyle(fontSize: _DESCRIPTION_SIZE * size.height),textAlign: TextAlign.center,),
+        ),
+        Container(
+          margin: EdgeInsets.only(top:_PANEL_TOP_ALINE * size.height),
+          child: Center(
+            child: Row(
+              children: [
+              _createPanels(context,PROFILE_KEY,PROFILE_THAME_COLOR,PROFILE_TITLE,"より詳細な情報を確認できます。"),
+              _createPanels(context,GAME_KEY,GAME_THAME_COLOR,GAME_TITLE,"Chronossの制作しているゲームを確認できます。"),
+              _createPanels(context,LIBRARY_KEY,LIBRARY_THAME_COLOR,LIBRARY_TITLE,"Chronossの制作しているライブラリを確認できます。"),
+            ],),
+          ),
         )
       ],),
       HOME_KEY,
